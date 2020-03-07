88b528c8cb1b913407d30b0e350e6aa3507bf0dc
joelsestrada2009@gmail.com <joelsestrada2009@gmail.com>
2020-03-07 10:11:59 -0400

    Primer Unit Test con Add

--- webapiCalculadora/utCalculadora/UnitTest1.cs
+++ webapiCalculadora/utCalculadora/UnitTest1.cs
@@ -1,13 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using webapiCalculadora.Controllers;
 
 namespace utCalculadora
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1Calculadora
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddGet1()
         {
+            //Arrange = Preparacion
+            CalculatorController calc = new CalculatorController();
+            int numa = 3;
+            int numb = 4;
+            int numres = 7;
+
+
+            //Act = Ejecucion
+            int result = calc.Add(numa, numb);
+            // Assert = Verificacion
+            Assert.AreEqual(numres, result);
         }
     }
 }
